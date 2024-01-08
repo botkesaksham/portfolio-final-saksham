83d392da203cb2107aaaeb6ae5088dbbdb976b19
botkesaksham <sa9607@srmist.edu.in>
2024-01-09 02:59:52 +0530

    about changes

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,13 @@ import Projects from '@/components/main/Projects'
 import Contact from '@/components/main/Contact'
 import Experience from "@/components/main/Experience"
 import Footer from '@/components/main/Footer'
+import About from '@/components/main/About'
 
 export default function Home() {
   return (
    <main className='h-full md:w-full'>
       <div className='flex flex-col gap-20'>
+          <About/>
           <Experience/>
           <Skills/>
           <Projects/>
